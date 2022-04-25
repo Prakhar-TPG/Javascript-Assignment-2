8535bb4fece1e86c10b97447113aa803ec9b25da
Prakhar-TPG <95743694+Prakhar-TPG@users.noreply.github.com>
2022-04-25 11:42:56 +0530

    Update Q-1.js

--- JS Assignment 2/Q-1.js
+++ JS Assignment 2/Q-1.js
@@ -5,22 +5,25 @@ class shape
     this.side=side;
     }
     getShape(){
-        return "Name of shape: "+ this.name+ "<br>"+" Number of side: "+ this.side
+        let total= "Name of shape: "+ this.name+ "<br>"+" Number of side: "+ this.side
+        console.log(total);
+        document.getElementById("Q1_results").innerHTML = total
+
     }
 }
 class rectangle extends shape
 {
-    constructor(length,width,name, side){
+    constructor(length,width,name,side){
         super(name,side)
         this.length=length
         this.width= width
     }
     newShape(){
-        return `Area of ${this.name}: `+this.length*this.width+" sq units"
+        let area=`Area of ${this.name}: `+this.length*this.width+" sq units"
+        console.log(area);
+        document.getElementById("Q1_results2").innerHTML = area
     }
 }
 let c1= new rectangle(15,25,"rectangle",4)
-console.log(c1.getShape())
-console.log(c1.newShape())
-document.getElementById("Q1_results").innerHTML = c1.getShape()
-document.getElementById("Q1_results2").innerHTML = c1.newShape()
+c1.getShape()
+c1.newShape()
